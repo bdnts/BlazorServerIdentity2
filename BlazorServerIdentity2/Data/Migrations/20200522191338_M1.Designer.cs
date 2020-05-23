ac5a4d3e07b4ba4fc21695857b09cd251795e5bc
Brian Douglass <brian.douglass@nowtimesystems.com>
2020-05-22 21:19:21 -0700

    Base Migration after creation.

--- BlazorServerIdentity2/Data/Migrations/20200522191338_M1.Designer.cs
+++ BlazorServerIdentity2/Data/Migrations/20200522191338_M1.Designer.cs
@@ -4,14 +4,16 @@ using BlazorServerIdentity2.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace BlazorServerIdentity2.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200522191338_M1")]
+    partial class M1
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
