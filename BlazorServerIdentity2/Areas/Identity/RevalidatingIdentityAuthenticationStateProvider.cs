729d2694a35921915ed2d21eaecb79ee9e47944d
Brian Douglass <brian.douglass@nowtimesystems.com>
2020-05-22 21:28:22 -0700

    --DI Logger for future. Added --CheckPasswordAsync() --CreateuserPrincipalAsync() --CreateAsync() --FindByNameAsync() --GetClaimsAsnc() --PasswordSignInAsync() --Refresh() --SignOutAsync() --ClaimComparer to do a variation of full out join: All of x plus any unique of y

--- BlazorServerIdentity2/Areas/Identity/RevalidatingIdentityAuthenticationStateProvider.cs
+++ BlazorServerIdentity2/Areas/Identity/RevalidatingIdentityAuthenticationStateProvider.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Security.Claims;
 using System.Threading;
 using System.Threading.Tasks;
@@ -17,6 +19,7 @@ namespace BlazorServerIdentity2.Areas.Identity
     {
         private readonly IServiceScopeFactory _scopeFactory;
         private readonly IdentityOptions _options;
+        private ILogger logger;
 
         public RevalidatingIdentityAuthenticationStateProvider(
             ILoggerFactory loggerFactory,
@@ -26,12 +29,13 @@ namespace BlazorServerIdentity2.Areas.Identity
         {
             _scopeFactory = scopeFactory;
             _options = optionsAccessor.Value;
+            logger = loggerFactory.CreateLogger("RevalidatingIdentityAuthenticationStateProvider");
         }
 
         protected override TimeSpan RevalidationInterval => TimeSpan.FromMinutes(30);
 
         protected override async Task<bool> ValidateAuthenticationStateAsync(
-            AuthenticationState authenticationState, CancellationToken cancellationToken)
+		    AuthenticationState authenticationState, CancellationToken cancellationToken)
         {
             // Get the user manager from a new scope to ensure it fetches fresh data
             var scope = _scopeFactory.CreateScope();
@@ -71,5 +75,227 @@ namespace BlazorServerIdentity2.Areas.Identity
                 return principalStamp == userStamp;
             }
         }
+        public async Task<bool> CheckPasswordAsync(TUser user, string password)
+        {
+            
+            // Get the user manager from a new scope to ensure it fetches fresh data
+            var scope = _scopeFactory.CreateScope();
+            try
+            {
+                var userManager = scope.ServiceProvider.GetRequiredService<UserManager<TUser>>();
+                var signInResult = await userManager.CheckPasswordAsync(user, password);
+                return signInResult;
+            }
+            finally
+            {
+                if (scope is IAsyncDisposable asyncDisposable)
+                {
+                    await asyncDisposable.DisposeAsync();
+                }
+                else
+                {
+                    scope.Dispose();
+                }
+            }
+        }
+        public async Task<ClaimsPrincipal> CreateUserPrincipalAsync(TUser user)
+        {
+            
+            // Get the user manager from a new scope to ensure it fetches fresh data
+            var scope = _scopeFactory.CreateScope();
+            try
+            {
+                var signInManager = scope.ServiceProvider.GetRequiredService<SignInManager<TUser>>();
+
+                var principal = await signInManager.CreateUserPrincipalAsync(user);
+                return principal;
+            }
+            catch(Exception ex)
+            {
+                return null;
+            }
+            finally
+            {
+                if (scope is IAsyncDisposable asyncDisposable)
+                {
+                    await asyncDisposable.DisposeAsync();
+                }
+                else
+                {
+                    scope.Dispose();
+                }
+            }
+        }
+
+        public  async Task<IdentityResult> CreateAsync(TUser user, string password)
+        {
+            
+            if (user == null) return IdentityResult.Failed(new IdentityError() { Code = "CreateAsyn 1", Description = "user is null" });
+            if (string.IsNullOrEmpty(password)) return IdentityResult.Failed(new IdentityError() { Code = "CreateAsyn 2", Description = "password is null" });
+
+
+            var scope = _scopeFactory.CreateScope();
+            try
+            {
+                var userManager = scope.ServiceProvider.GetRequiredService<UserManager<TUser>>();
+                var resultCreateUser = await userManager.CreateAsync(user);
+                return resultCreateUser;
+            }
+            finally
+            {
+                if (scope is IAsyncDisposable asyncDisposable)
+                {
+                    await asyncDisposable.DisposeAsync();
+                }
+                else
+                {
+                    scope.Dispose();
+                }
+            }
+        }
+        public async Task<TUser> FindByNameAsync(string username)
+        {
+            
+            if (string.IsNullOrEmpty(username)) return null;
+
+            // Get the user manager from a new scope to ensure it fetches fresh data
+            var scope = _scopeFactory.CreateScope();
+            try
+            {
+                var userManager = scope.ServiceProvider.GetRequiredService<UserManager<TUser>>();
+                var user = await userManager.FindByNameAsync(username);
+                return user;
+            }
+            finally
+            {
+                if (scope is IAsyncDisposable asyncDisposable)
+                {
+                    await asyncDisposable.DisposeAsync();
+                }
+                else
+                {
+                    scope.Dispose();
+                }
+            }
+        }
+
+        public async Task<IList<Claim>> GetClaimsAsync(TUser user)
+        {
+            if (user==null) return null;
+            // Get the user manager from a new scope to ensure it fetches fresh data
+            var scope = _scopeFactory.CreateScope();
+            try
+            {
+                var userManager = scope.ServiceProvider.GetRequiredService<UserManager<TUser>>();
+                var claims = await userManager.GetClaimsAsync(user);
+
+                return claims;
+            }
+            finally
+            {
+                if (scope is IAsyncDisposable asyncDisposable)
+                {
+                    await asyncDisposable.DisposeAsync();
+                }
+                else
+                {
+                    scope.Dispose();
+                }
+            }
+        }
+
+        public async Task<SignInResult> PasswordSignInAsync(string userName, string password, bool isPersistent, bool lockoutOnFailure)
+        {           
+            if (string.IsNullOrEmpty(userName)) return SignInResult.Failed;
+            if (string.IsNullOrEmpty(password)) return SignInResult.Failed;
+
+            var user = await FindByNameAsync(userName);
+            if (user == null) return SignInResult.Failed;
+
+            var claims = await GetClaimsAsync(user);
+            if (claims == null) return SignInResult.Failed;
+
+            var principal = await CreateUserPrincipalAsync(user);
+            if (principal == null) return SignInResult.Failed;
+
+            // Create new list with claims as principal
+            var resultU = claims.Union(principal.Claims, new ClaimComparer());
+
+            //var query = from claim in claims
+            //             pclaim in principal.Claims on claim.Type equals pclaim.Type
+            //            select new { claim.Type, pclaim.V}
+            var identity = new ClaimsIdentity(
+                resultU,
+                //principal.Claims,
+                //*************************
+                // This should be explored further to see if there is a better type.
+                Microsoft.AspNetCore.Authentication.Cookies.CookieAuthenticationDefaults.AuthenticationScheme
+            );
+            principal = new System.Security.Claims.ClaimsPrincipal(identity);
+            //This next statement is the heart at reflecting that the user is signed in.
+            this.SetAuthenticationState(Task.FromResult(new AuthenticationState(principal)));
+            // now the authState is updated
+            var authState = await GetAuthenticationStateAsync();
+            return SignInResult.Success;
+        }
+
+        public async Task<ClaimsPrincipal> Refresh()
+        {           
+            var CPUser = (await this.GetAuthenticationStateAsync()).User;
+            if (!CPUser.Identity.IsAuthenticated) return null;
+
+            var result = await PasswordSignInAsync(CPUser.Identity.Name, "x", false, false);
+            return CPUser;
+        }
+
+        public async Task<SignInResult> SignOutAsync()
+        {
+            var principal = new ClaimsPrincipal();
+            var identity = new ClaimsIdentity();
+            principal = new System.Security.Claims.ClaimsPrincipal(identity);
+
+            this.SetAuthenticationState(Task.FromResult(new AuthenticationState(principal)));
+            this.NotifyAuthenticationStateChanged(Task.FromResult(new AuthenticationState(principal)));
+            var authState = await GetAuthenticationStateAsync();
+            return SignInResult.Success;
+        }
     }
+
+    // Custom comparer for the Claim class
+    public  class ClaimComparer : IEqualityComparer<Claim>
+    {
+        // Claims are equal if their names and product numbers are equal.
+        public bool Equals(Claim x, Claim y)
+        {
+            //Check whether the compared objects reference the same data.
+            if (Object.ReferenceEquals(x, y)) return true;
+
+            //Check whether any of the compared objects is null.
+            if (Object.ReferenceEquals(x, null) || Object.ReferenceEquals(y, null)) return false;
+
+            // Check if duplicate type but of different value, don't want it.
+            if (x.Type == y.Type && x.Value != y.Value)
+                return true;
+            //Check whether the Claims' properties are equal.
+            //return x.Type == y.Type && x.Value == y.Value;
+            return x.Type == y.Type;
+        }
+
+        public int GetHashCode(Claim claim)
+        {
+            //Check whether the object is null
+            if (Object.ReferenceEquals(claim, null)) return 0;
+
+            //Get hash code for the Name field if it is not null.
+            int hashType = claim.Type == null ? 0 : claim.Type.GetHashCode();
+
+            //Get hash code for the Value field.
+            //int hashValue = claim.Value.GetHashCode();
+
+            //Calculate the hash code for the product.
+            //return hashType ^ hashValue;
+            return hashType;
+        }
+    }
+
 }
