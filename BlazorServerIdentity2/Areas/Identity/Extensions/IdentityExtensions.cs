7ace43025085c88ffbb01a3a5ea49cf064adc66c
Brian Douglass <brian.douglass@nowtimesystems.com>
2020-05-23 15:04:28 -0700

    Identity classes extenstion class, courtesy of Chris Sainty. For NavigationManager. Provides routine for getting attributes from URL.

---
+++ BlazorServerIdentity2/Areas/Identity/Extensions/IdentityExtensions.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.WebUtilities;
+using Microsoft.AspNetCore.Components;
+
+namespace BlazorServerIdentity2.Areas.Identity.Extensions
+{
+    public static class IdentityExtensions
+    {
+        // Courtesy Chris Sainty
+        #region NavigationManager
+        public static bool TryGetQueryString<T>(this NavigationManager navManager, string key, out T value)
+        {
+            var uri = navManager.ToAbsoluteUri(navManager.Uri);
+
+            if (QueryHelpers.ParseQuery(uri.Query).TryGetValue(key, out var valueFromQueryString))
+            {
+                if (typeof(T) == typeof(int) && int.TryParse(valueFromQueryString, out var valueAsInt))
+                {
+                    value = (T)(object)valueAsInt;
+                    return true;
+                }
+
+                if (typeof(T) == typeof(string))
+                {
+                    value = (T)(object)valueFromQueryString.ToString();
+                    return true;
+                }
+
+                if (typeof(T) == typeof(decimal) && decimal.TryParse(valueFromQueryString, out var valueAsDecimal))
+                {
+                    value = (T)(object)valueAsDecimal;
+                    return true;
+                }
+            }
+
+            value = default;
+            return false;
+        }
+        #endregion
+    }
+}
+
+
